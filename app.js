0c0de694feb335bddd5d15079cf5eb6078d3e14f
motumotu <motumotu@motumotu.xyz>
2015-11-22 20:15:47 +0900

    リプライ

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const BOT_ID = "moturoid";
 var twitter = require('twitter');
 var settings = require('./settings');
+require('date-utils');
+
 var bot = new twitter({
 	consumer_key        : settings.consumer_key,
 	consumer_secret     : settings.consumer_secret,
@@ -7,14 +10,49 @@ var bot = new twitter({
     access_token_secret : settings.access_token_secret
 });
 
+/*
+ * 
+ */
+setInterval(function() {
+
+}, 1000);
 
 /*
  * つぶやき
  */
-function tweet_text(text)
+function tweetText(text)
 {
-    bot.post('statuses/update', {status: text},  function(error, tweet, response){
+    bot.post('statuses/update', { status: text },  function(error, tweet, response){
         if(error) throw error;
         console.log("tweet ："+text);
     });
 }
+
+/*
+ * リプライ
+ */
+function tweetReply(text, reply_id)
+{
+    bot.post('statuses/update', { status: text, in_reply_to_status_id : reply_id }, function(error, tweet, response) {
+        if (error) throw error;
+        console.log("replay :" + text);
+    });
+}
+
+/*
+ * リプライに返事
+ */
+bot.stream('user', function(stream) {
+    stream.on('data', function(data) {
+        if (!('text' in data)) {
+            console.error('[ERROR] invalid data');
+            return;
+        }
+        var twUserId = data.user.screen_name;
+        var replayStr = data.text.replace(new RegExp('^@' + BOT_ID + ' '), '');
+        var isMention = (data.in_reply_to_user_id !== null);
+        var replyId = data.id_str;
+        if (!isMention || twUserId == BOT_ID) return;
+        tweetReply('@' + twUserId + ' ' + 'そうだね！', replyId);
+    });
+});
