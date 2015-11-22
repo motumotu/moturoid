b921543e6d8f982e62fd5ba970b7d397aee4d2c9
root <root@motumotu.xyz>
2015-11-23 01:56:22 +0900

    自動ツイート

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const BOT_ID = "moturoid";
 var twitter = require('twitter');
 var settings = require('./settings');
 require('date-utils');
+var noun = require('./noun_dic');
+//var kuromoji = require("kuromoji");
 
 var bot = new twitter({
 	consumer_key        : settings.consumer_key,
@@ -10,12 +12,13 @@ var bot = new twitter({
     access_token_secret : settings.access_token_secret
 });
 
+randTweet();
 /*
  * 
  */
 setInterval(function() {
-
-}, 1000);
+    randTweet();              // ランダムにツイート
+}, 600000);
 
 /*
  * つぶやき
@@ -56,12 +59,18 @@ bot.stream('user', function(stream) {
         
         // リプライに返信
         if (isMention && twUserId != BOT_ID) {
-            tweetReply('@' + twUserId + ' ' + 'それは ' + getAdjective() + " ね！", replyId);
+            tweetReply('@' + twUserId + ' ' + 'それは' + getAdjective() + "ね！", replyId);
         }
         // あいさつ
         else if (text.match(/おはよう/)) {
             tweetReply('@' + twUserId + ' ' + 'おはよーo(^-^)o', replyId);
         }
+        /*
+        kuromoji.builder({ dicPath: "./node_modules/kuromoji/dist/dict/" }).build(function(err, tokenizer) {
+            var path = tokenizer.tokenize(text);
+            console.log(path);
+        });
+        */
     });
 });
 
@@ -70,7 +79,7 @@ bot.stream('user', function(stream) {
  */
 function getNoun()
 {
-    dic = ["リンゴ"];
+    var dic = noun.noun_dic;
     return dic[Math.floor(Math.random() * dic.length)];
 }
 
@@ -79,7 +88,38 @@ function getNoun()
  */
 function getAdjective()
 {
-    var dic = ["嬉しい", "楽しい", "苦しい", "つらい", "悲しい", "恐ろしい", "寂しい", "面白い", "凄い"];
+    var dic = ["嬉しい", "楽しい", "苦しい", "つらい", "悲しい", "恐ろしい", "寂しい", "面白い", "凄い", "強い", "弱い", "偉い", "かわいい", "暑い", "甘い", "荒い", "軽い", "重い", "速い", "遅い", "丸い", "臭い", "寒い", "美しい", "白い", "赤い", "青い", "黒い", "黄色い", "美味しい", "美味しくない", "悪い", "正しい", "渋い", "若い", "太い", "細い", "怪しい", "うるさい", "おかしい", "厳しい", "悔しい", "詳しい", "そこはかとない", "空恐ろしい", "かたじけない", "おとなしい」", "こうばしい", "騒がしい", "しんどい", "大きい", "小さい"];
     return dic[Math.floor(Math.random() * dic.length)];
 }
 
+/*
+ * ランダムツイート
+ */
+function randTweet()
+{
+    var text;
+    var rnd = Math.floor(Math.random() * 20);
+    switch (rnd) {
+        case 0: text = getNoun() + "って" + getAdjective() + "よねぇ"; break;
+        case 1: text = getNoun() + "が" + getAdjective() + "らしいよ！"; break;
+        case 2: text = getNoun() + "が食べたいなぁ"; break;
+        case 3: text = "今日は" + getNoun() + "しようかな"; break;
+        case 4: text = getNoun() + "って私苦手なのよねぇー"; break;
+        case 5: text = getAdjective() + "ときは、やっぱり" + getNoun() + "だよね！"; break;
+        case 6: text = "最近のマイブームは" + getNoun() + "だよ！"; break;
+        case 7: text = getNoun() + "と" + getNoun() + "って似てるよね"; break;
+        case 8: text = getAdjective() + getNoun() + "って" + getAdjective() + "よね！"; break;
+        case 9: text = "あんなに" + getAdjective() + "なんて・・・"; break;
+        case 10: text = getAdjective() + "のが好きなの"; break;
+        case 11: text = "たまには" + getNoun() + "しないとね！"; break;
+        case 12: text = "今日は" + getAdjective() + "気分だよ！"; break;
+        case 13: text = getAdjective() + getNoun() + "ってどう思う？"; break;
+        case 14: text = "今から" + getNoun() + "するよ！"; break;
+        case 15: text = getNoun() + "を見たよ！"; break;
+        case 16: text = getNoun() + "よかったよ！"; break;
+        case 17: text = getAdjective() + "よぉ"; break;
+        case 18: text = getNoun() + "と" + getNoun() + "は" + getNoun() + "なんだよ！"; break;
+        case 19: text = getNoun() + "の特集を見たよ！"; break;
+    }
+    tweetText(text);
+}
